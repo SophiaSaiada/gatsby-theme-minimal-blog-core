8018f1f2c111ec138b1beb08be31eec12104a50c
Sophia Saiada <sophia.saiada@gmail.com>
2020-01-09 17:10:59 +0200

    Add banner to homepage posts

--- src/templates/homepage-query.tsx
+++ src/templates/homepage-query.tsx
@@ -5,11 +5,18 @@ export default HomepageComponent
 
 export const query = graphql`
   query {
-    allPost(sort: { fields: date, order: DESC }, limit: 3) {
+    allPost(sort: { fields: date, order: DESC }, limit: 15) {
       nodes {
         slug
         title
         date(formatString: "DD.MM.YYYY")
+        banner {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
         tags {
           name
           slug
